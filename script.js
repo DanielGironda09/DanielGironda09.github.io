ed17effa7651ce76862cd81eaca766b7736a3dc2
DanielGironda09 <dany.600.girondazet@gmail.com>
2024-11-05 14:46:51 -0400

    Sitio Web

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuItems = document.querySelectorAll(".menu-item");
-    const sections = document.querySelectorAll("section");
-
-    // Función para mostrar la sección correspondiente
-    menuItems.forEach(item => {
-        item.addEventListener("click", function (event) {
-            event.preventDefault(); // Evitar que el enlace se desplace automáticamente
-
-            // Ocultar todas las secciones
-            sections.forEach(section => {
-                section.classList.add("hidden");
-            });
-
-            // Mostrar la sección correspondiente
-            const targetId = item.getAttribute("href");
-            const targetSection = document.querySelector(targetId);
-            targetSection.classList.remove("hidden");
-        });
-    });
-});
+function navigateTo(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
